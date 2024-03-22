1e24fa03ff8f7dd9a854fa668e45299b2e4c1a8b
Ramkumar R <rra@exerp.com>
2024-03-22 23:14:39 +0530

    typescript fix

--- lib/components/PDFPage.vue
+++ lib/components/PDFPage.vue
@@ -11,27 +11,30 @@
   </div>
 </template>
 
-<script>
+<script lang="ts">
 import { ref, onMounted, onBeforeUnmount } from "vue";
 
 export default {
   props: {
     page: Object,
   },
-  setup(props, { emit }) {
-    const canvas = ref(null);
+  setup(
+    props: Readonly<{ [key: string]: any }>,
+    { emit }: { emit: (event: string, ...args: any[]) => void }
+  ) {
+    const canvas = ref<HTMLCanvasElement | null>(null);
     const width = ref(0);
     const height = ref(0);
 
     const measure = () => {
       emit("measure", {
-        scale: canvas.value.clientWidth / width.value,
+        scale: canvas.value!.clientWidth / width.value,
       });
     };
 
     onMounted(async () => {
       const _page = await props.page;
-      const context = canvas.value.getContext("2d");
+      const context = canvas.value!.getContext("2d");
       const viewport = _page.getViewport({ scale: 1, rotation: 0 });
       width.value = viewport.width;
       height.value = viewport.height;
@@ -44,7 +47,7 @@ export default {
           console.log("Page rendered");
         });
       emit("measure", {
-        scale: canvas.value.clientWidth / width.value,
+        scale: canvas.value!.clientWidth / width.value,
       });
       window.addEventListener("resize", measure);
     });
