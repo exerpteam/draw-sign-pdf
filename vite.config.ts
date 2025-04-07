14bd1d4e99a550c44f8aac0513c1e08a9b3764c0
akshayk181097 <149584401+akshayk181097@users.noreply.github.com>
2025-04-07 08:18:13 +0530

    fix 5

--- vite.config.ts
+++ vite.config.ts
@@ -74,7 +74,8 @@ const prodConfig = defineConfig({
             vue: 'Vue',
           },
           dir: 'dist/umd',
-          exports: 'named'
+          exports: 'named',
+          inlineDynamicImports: true
         },
       ],
     },
