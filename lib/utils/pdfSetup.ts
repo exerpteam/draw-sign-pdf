3ed87500355aae5cb36b400b5ad92d098ad9491a
akshayk181097 <149584401+akshayk181097@users.noreply.github.com>
2025-04-08 15:43:37 +0530

    package changes

---
+++ lib/utils/pdfSetup.ts
@@ -0,0 +1,8 @@
+import pdfjsLib from 'pdfjs-dist';
+
+export const initializePdfjs = () => {
+  pdfjsLib.GlobalWorkerOptions.workerSrc = new URL(
+    'pdfjs-dist/build/pdf.worker.min.mjs',
+    import.meta.url
+  ).toString();
+}; 
